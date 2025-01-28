637457bc5fe01e8688d59da2c0d5d2e2bddcc811
Shafeequemk80 <64883556+Shafeequemk80@users.noreply.github.com>
2025-01-28 22:06:42 +0530

    c

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
-
+console.log(process.env.FRONDEND)
 app.use(cors({
   origin: process.env.FRONDEND
 }));
